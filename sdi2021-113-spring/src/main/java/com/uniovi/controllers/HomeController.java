dfd90c376d1456da9e0482948e5fa24a07442f7a
Ramón Vila <ramon.vilafer@gmail.com>
2021-02-17 10:26:15 +0100

    SDI–2021-113-session2-Ejercicio-complementario2

--- sdi2021-113-spring/src/main/java/com/uniovi/controllers/HomeController.java
+++ sdi2021-113-spring/src/main/java/com/uniovi/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.uniovi.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
--- sdi2021-113-spring/src/main/java/com/uniovi/services/ProfessorsService.java
+++ sdi2021-113-spring/src/main/java/com/uniovi/services/ProfessorsService.java
@@ -2,48 +2,34 @@ package com.uniovi.services;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.uniovi.entities.Professor;
+import com.uniovi.repositories.ProfessorsRepository;
 
 @Service
 public class ProfessorsService {
-
-	private List<Professor> profesores = new ArrayList<Professor>();
+	
+	@Autowired
+	ProfessorsRepository professorsRepository;
 
 	public List<Professor> getProfessors() {
-		if (profesores == null) {
-			profesores.add(new Professor(1L, "122335566F", "Andrés", "Fernández", "Doctor"));
-			profesores.add(new Professor(2L, "981553910A", "José", "De la Cruz", "Temporal"));
-			profesores.add(new Professor(3L, "798719823G", "María", "Martínez", "Catedrática"));
-		}
+		List<Professor> profesores = new ArrayList<Professor>();
+		professorsRepository.findAll().forEach(profesores::add); 
 		return profesores;
 	}
 
 	public Professor getProfessor(Long id) {
-		if (profesores != null) {
-			for (Professor p : profesores) {
-				if (p.getId() == id) return p;
-			}
-		}
-		return null;
+		return professorsRepository.findById(id).get();
 	}
 
 	public void addProfessor(Professor prof) {
-		if (prof.getId() == null) {
-			// Obtener el último ID de la lista
-			long last_id = profesores.get( profesores.size() -1 ).getId();
-			prof.setId( last_id + 1 );
-			profesores.add( prof );
-		}
+		professorsRepository.save(prof);
 	}
 
 	public void deleteProfessor(Long id) {
-		for (int i= 0; i < profesores.size(); i++) {
-			Professor current_prof = profesores.get(i);
-			if (current_prof.getId() == id) {
-				profesores.remove(i);
-			}
-		}
+		professorsRepository.deleteById(id);
 	}
 }
