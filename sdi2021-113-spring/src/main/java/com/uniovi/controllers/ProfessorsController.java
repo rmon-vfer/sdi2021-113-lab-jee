ca8f284334729027721e8caceb0bb5939a007a39
Ramón Vila <ramon.vilafer@gmail.com>
2021-02-17 11:48:23 +0100

    SDI-2021-113-session2-Ejercicio-complementario3

--- sdi2021-113-spring/src/main/java/com/uniovi/controllers/ProfessorsController.java
+++ sdi2021-113-spring/src/main/java/com/uniovi/controllers/ProfessorsController.java
@@ -17,47 +17,41 @@ import com.uniovi.services.ProfessorsService;
  * Spring trata todas las peticiones como GET por defecto,
  * para responder a peticiones POST hay que especificarlo
  * en la anotación @RequestMapping */
-@RestController
+@Controller
 public class ProfessorsController {
 
 	@Autowired // Inyectar el servicio
 	private ProfessorsService professorsService;
 
 	// Ver listado
-	@RequestMapping("/professors/list")
-	public String getList() {
-		StringBuffer sb = new StringBuffer();
-		List<Professor> current_professors = professorsService.getProfessors();
-		
-		sb.append( "Professors on database: " + current_professors.size() + "\n");
-		for (Professor p : current_professors) {
-			sb.append(p.toString() + "\n");
-		}
-		return sb.toString();
+	@RequestMapping("/professor/list")
+	public String getList(Model model) {
+		model.addAttribute("professorsList", professorsService.getProfessors());
+		return "professor/list"; 
 	}
 	
 	// Añadir
-	@RequestMapping(value = "/professors/add", method = RequestMethod.POST)
+	@RequestMapping(value = "/professor/add", method = RequestMethod.POST)
 	public String addProfessor( @ModelAttribute Professor prof ) {
 		professorsService.addProfessor(prof);
-		return "Added professor with dni: " + prof.getDni();
+		return "redirect:/professor/add";
 	}
 	
 	// Ver detalle
-	@RequestMapping("/professors/details/{id}")
+	@RequestMapping("/professor/details/{id}")
 	public String getDetail( @PathVariable Long id) {
 		return "Details for professor with id " + id + ": " +  professorsService.getProfessor(id).toString();
 	}
 	
 	// Eliminar
-	@RequestMapping("/mark/delete/{id}")
-	public String deleteMark(@PathVariable Long id) {
+	@RequestMapping("/professor/delete/{id}")
+	public String deleteMark(@RequestParam Long id) {
 		professorsService.deleteProfessor(id);
-		return "Deleted professor with id " + id;
+		return "redirect:/professor/remove";
 	}
 	
 	// Editar
-	@RequestMapping(value = "/mark/edit/{id}", method = RequestMethod.POST)
+	@RequestMapping(value = "/professor/edit/{id}", method = RequestMethod.POST)
 	public String setEdit(@PathVariable Long id, @ModelAttribute Professor professor) {
 		professorsService.deleteProfessor(id);
 		professor.setId(id);
