0470ef6a127c5dcc179880c9f0f1323bfb85d079
Ramón Vila <ramon.vilafer@gmail.com>
2021-03-06 17:43:56 +0100

    SDI-2021-113-5.4-Casos Uso VistaHome.

--- sdi2021-113-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi2021-113-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -6,39 +6,42 @@ import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NotaneitorTests {
-	
+
 	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
 	static String Geckdriver024 = "C:\\Users\\ramon\\geckodriver024win64.exe";
 
 	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
 	static String URL = "http://localhost:8090";
-	
+
 	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
 		System.setProperty("webdriver.firefox.bin", PathFirefox);
 		System.setProperty("webdriver.gecko.driver", Geckdriver);
 		WebDriver driver = new FirefoxDriver();
-		
+
 		return driver;
 	}
-	
 
 	// Antes de cada prueba se navega al URL home de la aplicación
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
 		driver.navigate().to(URL);
 	}
 
-	// Después de cada prueba se borran las cookies del navegador
 	@After
-	public void tearDown() {
+	public void tearDown() throws Exception {
 		driver.manage().deleteAllCookies();
 	}
 
 	// Antes de la primera prueba
 	@BeforeClass
 	static public void begin() {
+
 	}
 
 	// Al finalizar la última prueba
@@ -48,8 +51,31 @@ public class NotaneitorTests {
 		driver.quit();
 	}
 
+	// PR01. Acceder a la página principal /
+	@Test	
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
+	}
+
+	// PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+
+	// PR03. OPción de navegación. Pinchar en el enlace Identificate en la página
+	// home
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+
+	// PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a
+	// Español
+	@Test
+	public void PR04() {
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(),
+				PO_Properties.getENGLISH());
+		// SeleniumUtils.esperarSegundos(driver, 2);
 	}
 }
